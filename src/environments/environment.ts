79a2d0de6b537b10290ae3ee872c9b19e17cde97
fpolania <lassofabian36@gmail.com>
2026-05-31 09:35:29 -0500

    ajustes

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,1 +1,12 @@
-export const environment = {};
+export const environment = {
+  production: false,
+  firebase: {
+    apiKey: 'AIzaSyA9XuIH6Z9Fhn2ZQJU2SJTYf9k1fRt6gEE',
+    authDomain: 'campocontrol-de38b.firebaseapp.com',
+    projectId: 'campocontrol-de38b',
+    storageBucket: 'campocontrol-de38b.firebasestorage.app',
+    messagingSenderId: '35685033278',
+    appId: '1:35685033278:web:6c2ad9b04826447f4b786f',
+    measurementId: 'G-2NZLYG1T99',
+  },
+};
